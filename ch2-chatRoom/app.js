781bc6cbe2b4817b54413013cd6313900e45d70f
valoon3 <valoon2@naver.com>
2022-11-17 22:36:42 +0900

    cookies 설정

--- ch2-chatRoom/app.js
+++ ch2-chatRoom/app.js
@@ -2,10 +2,16 @@ const express = require('express');
 const morgan = require('morgan');
 const { sequelize } = require('./models/index');
 const nunjuncks = require('nunjucks');
+const session = require('express-session');
 const passportConfig = require('./passport');
+const cookieParser = require('cookie-parser');
+const passport = require('passport');
 
 const app = express();
+const passportConfig = require('./passport/index');
+const authRouter = require('./routes/auth');
 
+passportConfig();
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'html'); // 템플릿엔진 설정
 nunjuncks.configure('views', {
@@ -21,13 +27,26 @@ sequelize.sync({ force: true })
         console.error(err);
     })
 
+const sessionMiddleware = session({
+    resave: false,
+    saveuninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+        httpOnly: true,
+        secure: false,
+    },
+})
+
 app.use(morgan('dev'));
 app.use(express.json);
 app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(sessionMiddleware);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.use((req, res, next) => {
-
-})
+// app.use('/', indexRouter);
+// app.use('')
 
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
