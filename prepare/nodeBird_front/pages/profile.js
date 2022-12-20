019cb8f3b14412b2a96f06f034cdf15cb50495d1
valoon3 <valoon2@naver.com>
2022-12-20 20:15:41 +0900

    profile 닉네임 수정 팔로우, 팔로잉 리스트 포맷

--- prepare/nodeBird_front/pages/profile.js
+++ prepare/nodeBird_front/pages/profile.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import Head from "next/head";
+
 import AppLayout from "../components/AppLayout";
+import NicknameEditForm from "../components/profile/NicknameEditForm";
+import FollowList from "../components/profile/FollowList";
 
 const Profile = () => {
+
+    // 더미 데이터
+    const followingList = [
+        {nickname: '제로초'},
+        {nickname: '나병호'},
+        {nickname: '노드버드 오피셜'},
+    ];
+    const followerList = [
+        {nickname: '제로초'},
+        {nickname: '나병호'},
+        {nickname: '노드버드 오피셜'},
+    ]
+
     return (
         <div>
             <Head> {/* Head 설정을 건드리고 싶을때에는 next의 head 컴포넌트를 사용한다. */}
                 <title>프로필</title>
             </Head>
-            <AppLayout><div>내 프로필</div></AppLayout>
+            <AppLayout>
+                <NicknameEditForm />
+                <FollowList header="팔로잉 목록" data={followingList}/>
+                <FollowList header="팔로워 목록" data={followerList}/>
+            </AppLayout>
 
         </div>
     )
