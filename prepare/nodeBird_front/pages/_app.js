e96368a4f8b17cdeebc1b2fe29dffa0597254c89
valoon3 <valoon2@naver.com>
2022-12-22 03:24:30 +0900

    _app.js 설명 주석 추가

--- prepare/nodeBird_front/pages/_app.js
+++ prepare/nodeBird_front/pages/_app.js
@@ -3,6 +3,10 @@ import Head from 'next/head';
 
 import wrapper from "../store/configureStore";
 
+// _app은 서버로 요청이 들어왔을 때 가장 먼저 실행되는 컴포넌트
+// 페이지에 적용할 공통 레이아웃의 역할은 한다.
+
+
 const NodeBird = ({ Component }) => {
     return (
         <>
