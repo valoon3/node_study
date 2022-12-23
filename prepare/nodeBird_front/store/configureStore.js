864d598498ccde8192b71403fa159d9e2be89a5f
valoon3 <valoon2@naver.com>
2022-12-24 02:36:09 +0900

    리덕스 Action 콘솔로그 찍기  + 메뉴 사라진거 복구

--- prepare/nodeBird_front/store/configureStore.js
+++ prepare/nodeBird_front/store/configureStore.js
@@ -1,11 +1,23 @@
 import {createWrapper} from "next-redux-wrapper";
 import {applyMiddleware, compose, createStore} from "redux";
+import thunkMiddleware from 'redux-thunk';
+
 import reducer from "./reducer";
 import {composeWithDevTools} from "redux-devtools-extension";
 
+// customMiddleware
+const loggerMiddleware = ({ dispatch, getState }) => (next) => (action) => {
+    console.log(action);
+
+    return next(action);
+}
+
+const testMiddleware = ({dispatch, getState}) => {
+
+}
 
 const configureStore = () => {
-    const middlewares = [];
+    const middlewares = [thunkMiddleware, loggerMiddleware]; // thunkMiddleware는 함수로 들어온 입력도 처리 가능하도록 만들어줌
     // redux 개발자 도구 사용을 위한 설정
     const enhancer = process.env.NODE_ENV === 'production' ?
             compose(applyMiddleware(...middlewares)) : // production env
