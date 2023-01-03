91afebf0b84d6ac67c65322cbf71932b88bc136b
valoon3 <valoon2@naver.com>
2023-01-03 21:23:20 +0900

    immer 적용 post

--- prepare/nodeBird_front/store/reducer/post.js
+++ prepare/nodeBird_front/store/reducer/post.js
@@ -1,5 +1,6 @@
 import shortId from "shortid";
 import {ADD_POST_TO_ME, REMOVE_POST_OF_ME} from "./user";
+import produce from "immer";
 
 export const initialState = {
     mainPosts: [
@@ -107,93 +108,148 @@ const dummyComment = (data) => ({
 });
 
 const reducer = (state = initialState, action) => {
-    switch(action.type) {
-        case ADD_POST_REQUEST:
-            return {
-                ...state,
-                addPostLoading: true,
-                addPostDone: false,
-                addPostError: null,
-            };
-        case ADD_POST_SUCCESS:
-            return {
-                ...state,
-                mainPosts: [dummyPost(action.data), ...state.mainPosts],
-                addPostLoading: false,
-                addPostDone: true,
-            };
-        case ADD_POST_TO_ME:
-            return {
-                ...state,
-                me: {
-                    ...state.me,
+
+    // immer의 produce가 자동으로 state를 draft로 만들어서 불변성을 유지시켜준다.
+    return produce(state, (draft) => {
+
+        switch(action.type) {
+            case ADD_POST_REQUEST:
+                draft.addPostLoading = true;
+                draft.addPostDone = false;
+                draft.addPostError = null;
+                break;
+
+                // return {
+                //     ...state,
+                //     addPostLoading: true,
+                //     addPostDone: false,
+                //     addPostError: null,
+                // };
+            case ADD_POST_SUCCESS:
+                draft.addPostLoading = false;
+                draft.addPostDone = true;
+                draft.mainPosts.unshift(dummyPost(action.data));
+                break;
+
+                // return {
+                //     ...state,
+                //     mainPosts: [dummyPost(action.data), ...state.mainPosts],
+                //     addPostLoading: false,
+                //     addPostDone: true,
+                // };
+            case ADD_POST_FAILURE:
+                draft.addPostLoading = false;
+                draft.addPostError = action.error;
+                break;
+
+                // return {
+                //     ...state,
+                //     addCommentLoading: false,
+                //     addCommentError: action.error,
+                // };
+            case ADD_COMMENT_REQUEST:
+                draft.addCommentLoading = true;
+                draft.addCommentDone = false;
+                draft.addCommentError = null;
+                break;
+                // return {
+
+            //     ...state,
+            //     addCommentLoading: true,
+            //     addCommentDone: false,
+            //     addCommentError: null,
+            // };
+            case ADD_COMMENT_SUCCESS:
+                const post = draft.mainPosts.find((v) => v.id === action.data.postId);
+                post.Comments.unshift(dummyComment(action.data.content));
+                draft.addCommentLoading = false;
+                draft.addCommentDone = true;
+                break;
+                // const postIndex = state.mainPosts.findIndex((v) => v.id === action.data.postId);
+
+            // const post = {...state.mainPosts[postIndex]};
+            // post.Comments = [dummyComment(action.data.content), ...post.Comments];
+            // const mainPosts = [...state.mainPosts];
+            // mainPosts[postIndex] = post;
+            //
+            // return {
+            //     ...state,
+            //     mainPosts,
+            //     addCommentLoading: false,
+            //     addCommentDone: true,
+            // };
+            case ADD_COMMENT_FAILURE:
+                draft.addCommentLoading = false;
+                draft.addCommentError = action.error;
+                break;
+                // return {
+
+            //     ...state,
+            //     addCommentLoading: false,
+            //     addCommentError: action.error,
+            // };
+            case REMOVE_POST_REQUEST:
+                draft.removePostLoading = true;
+                draft.removePostDone = false;
+                draft.removePostError = null;
+                break;
+            // return {
+            //     ...state,
+            //     removePostLoading: true,
+            //     removePostDone: false,
+            //     removePostError: null,
+            // };
+            case REMOVE_POST_SUCCESS:
+                draft.removePostLoading = false;
+                draft.removePostDone = true;
+                draft.mainPosts = state.mainPosts.filter((v) => v.id !== action.data);
+                break;
+                // return {
+
+            //     ...state,
+            //     mainPosts: state.mainPosts.filter((v) => v.id !== action.data),
+            //     removePostLoading: false,
+            //     removePostDone: true,
+            // };
+            case REMOVE_POST_FAILURE:
+                draft.removePostLoading = false;
+                draft.removePostError = action.error;
+                break;
+                // return {
+
+            //     ...state,
+            //     removePostLoading: true,
+            //     removePostDone: false,
+            //     removePostError: null,
+            // };
+            case ADD_POST_TO_ME:
+                draft.me = {
                     Posts: [{id: action.data}, ...state.me.Posts],
                 }
-            }
-        case ADD_POST_FAILURE:
-            return {
-                ...state,
-                addCommentLoading: false,
-                addCommentError: action.error,
-            };
-        case ADD_COMMENT_REQUEST:
-            return {
-                ...state,
-                addCommentLoading: true,
-                addCommentDone: false,
-                addCommentError: null,
-            };
-        case ADD_COMMENT_SUCCESS:
-            const postIndex = state.mainPosts.findIndex((v) => v.id === action.data.postId);
-            const post = {...state.mainPosts[postIndex]};
-            post.Comments = [dummyComment(action.data.content), ...post.Comments];
-            const mainPosts = [...state.mainPosts];
-            mainPosts[postIndex] = post;
+                break;
 
-            return {
-                ...state,
-                mainPosts,
-                addCommentLoading: false,
-                addCommentDone: true,
-            };
-        case ADD_COMMENT_FAILURE:
-            return {
-                ...state,
-                addCommentLoading: false,
-                addCommentError: action.error,
-            };
-        case REMOVE_POST_REQUEST:
-            return {
-                ...state,
-                removePostLoading: true,
-                removePostDone: false,
-                removePostError: null,
-            };
-        case REMOVE_POST_SUCCESS:
-            return {
-                ...state,
-                mainPosts: state.mainPosts.filter((v) => v.id !== action.data),
-                removePostLoading: false,
-                removePostDone: true,
-            };
-        case REMOVE_POST_FAILURE:
-            return {
-                ...state,
-                removePostLoading: true,
-                removePostDone: false,
-                removePostError: null,
-            };
-        case REMOVE_POST_OF_ME:
-            return {
-                ...state,
-                me: {
-                    ...state.me,
-                    Posts: state.me.Posts.filter((v) => v.id === action.data),
-                },
-            };
-        default:
-            return state;
-    }
+            // return {
+            //     ...state,
+            //     me: {
+            //         ...state.me,
+            //         Posts: [{id: action.data}, ...state.me.Posts],
+            //     }
+            // }
+            case REMOVE_POST_OF_ME:
+                return {
+                    ...state,
+                    me: {
+                        ...state.me,
+                        Posts: state.me.Posts.filter((v) => v.id === action.data),
+                    },
+                };
+            default:
+                return state;
+        }
+
+
+    });
+
 }
 
 export default reducer;
