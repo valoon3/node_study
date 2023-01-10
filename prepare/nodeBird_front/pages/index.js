77abd791fe0e2edb3c709cf9acee95dee289dbf3
valoon3 <valoon2@naver.com>
2023-01-10 22:17:44 +0900

    front faker 문제 해결해야함

--- prepare/nodeBird_front/pages/index.js
+++ prepare/nodeBird_front/pages/index.js
@@ -15,6 +15,18 @@ const Home = () => {
         })
     }, []);
 
+    useEffect(() => {
+        function onScroll() {
+            console.log(window.scrollY, document.documentElement.clientHeight, document.documentElement.scrollHeight);
+        }
+
+        window.addEventListener('scroll', onScroll);
+
+        return () => { // 삭제하지 않으면 계속 메모리에 쌓여있는다.
+            window.removeEventListener('scroll', onScroll);
+        };
+    });
+
     const { me } = useSelector(state => state.user);
     const { mainPosts } = useSelector(state => state.post);
 
