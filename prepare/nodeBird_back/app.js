1ce4f7988024baa1756aca59dfc511f5e9cfee36
valoon3 <valoon2@naver.com>
2023-01-11 22:58:21 +0900

    express router 분리 수업

--- prepare/nodeBird_back/app.js
+++ prepare/nodeBird_back/app.js
@@ -1,9 +1,19 @@
-const http = require('http');
+const express = require('express');
 
-http.createServer((req, res) => {
-    console.log(req.url, res.method);
-    res.end('hello node');
-})
+const app = express();
+const postRouter = require('./routes/post');
+
+app.use('post', postRouter)
+
+app
+    .get('/', (req, res) => {
+        res.send('hello express');
+    })
+    .get('/', (req, res) => {
+        res.send('hello api');
+    })
+
+
     .listen(3065, () => {
         console.log('서버 실행 중');
     });
